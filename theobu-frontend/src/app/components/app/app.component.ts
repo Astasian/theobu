73d74858ac31b3aed31094c899cecd9efeb87f47
Astasian <astasian@users.noreply.github.com>
2019-04-26 23:58:20 +0200

    Added update service

--- theobu-frontend/src/app/components/app/app.component.ts
+++ theobu-frontend/src/app/components/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
-import { Language } from '../../models/language';
 import { MatDialog } from '@angular/material/dialog';
 import { SettingsDialogComponent } from '../settings-dialog/settings-dialog.component';
+import { UpdateService } from 'src/app/services/update.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,11 @@ import { SettingsDialogComponent } from '../settings-dialog/settings-dialog.comp
 })
 export class AppComponent {
 
-  constructor(translate: TranslateService, private languageService: LanguageService, public dialog: MatDialog) {
+  constructor(
+    translate: TranslateService,
+    private languageService: LanguageService,
+    public dialog: MatDialog,
+    private updateService: UpdateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
