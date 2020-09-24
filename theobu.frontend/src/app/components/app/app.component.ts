0ebaf8f3403213afb71597de8997a63af5de2285
Astasian <Astasian@users.noreply.github.com>
2020-09-24 23:53:12 +0200

    updates

--- theobu.frontend/src/app/components/app/app.component.ts
+++ theobu.frontend/src/app/components/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-
 @Component({
   selector: 'tb-root',
   templateUrl: './app.component.html',
--- theobu.frontend/src/app/services/contentful.service.ts
+++ theobu.frontend/src/app/services/contentful.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { createClient, ContentfulClientApi, CreateClientParams } from 'contentful';
 import { ICard } from '../models/card';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -8,23 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ContentfulService {
 
-  private CONFIG: CreateClientParams = {
-    space: '34qzpf8upmny',
-    accessToken: 'RwHFsICsB49aPqrQwmdd4lmOuMhn3l1vTbwWrwY_bJU'
-  };
-
-  private _client: ContentfulClientApi;
-  private _language: string = 'de';
-
-  constructor(private translateService: TranslateService) {
-    this._client = createClient(this.CONFIG);
-
-  //  this._language = translateService.currentLang;
-  //  translateService.onTranslationChange.subscribe(l => this._language = l);
-  }
-
   async getAllCards(): Promise<ICard[]> {
-    const result = await this._client.getEntries<ICard>({locale: this._language});
-    return result.items.map(c => c.fields);
+   return Promise.resolve([]);
   }
 }
